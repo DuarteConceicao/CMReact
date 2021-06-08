d1677a234fb1527a3fb9359f81d224eb271388fa
Duarte Conceição <190221077@estudantes.ips.pt>
2021-06-08 19:07:07 +0100

    Login Comecado

--- App.js
+++ App.js
@@ -17,6 +17,7 @@ import {
   TextInput,
   useColorScheme,
   View,
+  Button,
 } from 'react-native';
 
 import {
@@ -28,61 +29,27 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 import { TestScheduler } from '@jest/core';
 
-export default function App(){
+var styles = require('./components/LoginStyle.js');
+
+
+const App: () => React$Node = () => {
   return (
-      <View style={styles.title}>
-        <Text>Calendários</Text>
+    <View style={styles.background}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Sempre a Horas</Text>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Username:</Text>
+          <TextInput style={styles.inputMargin}></TextInput>
         </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Password:</Text>
+          <TextInput style={styles.inputMargin}></TextInput>
+          <Button title="Entrar" style={styles.button}/>
+        </View>
+      </View>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  title: {
-    width: '100%',
-    backgroundColor: 'blue',
-    height: '100%',
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'column',
-    backgroundColor: '#ffffff',
-  },
-  center: {
-    marginTop: 20,
-    alignItems: 'center',
-  },
-  group: {
-    marginTop: 20,
-  },
-  label: {
-    color: "black",
-    fontSize: 20,
-    textAlign: 'left',
-    margin: 10,
-  },
-  input: {
-    padding: 10,
-    height: 40,
-    borderWidth: 0,
-  },
-  button: {
-    marginTop: 20,
-    backgroundColor: '#ff3300',
-    padding: 5,
-    borderWidth: 2,
-    borderColor: "#ff3300",
-    borderRadius: 6, 
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 25,
-    textAlign: 'center',
-  },
-  result: {
-    fontSize: 50,
-    textAlign: 'center',
-    color: '#000000',
-    margin: 10,
-  },
-});
+
+export default App;
